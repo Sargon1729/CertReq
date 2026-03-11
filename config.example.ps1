8f1924fefa28cf11bc5e8b073d1cca5557ebe42e
Sargon1729 <136961479+Sargon1729@users.noreply.github.com>
2026-03-11 10:37:25 +0000

    Update PKI root and CA name in config example

--- config.example.ps1
+++ config.example.ps1
@@ -1,5 +1,3 @@
-$PKIRoot = '\\bss-pri-qna01\backups\PKI'                            # PKI root folder
-$certfolder = ".\Signed Certs"                                      # signed certs folder
-$Completed_csr_path = '.\Completed CSR'                             # completed CSRs
-$ca = 'Barrett Steel Issuing CA'                                    # name of the CA
-$LOGFILE = ".\LOG_CertReq_PS.txt"                                   # Log file location
+$PKIRoot = '\path-to\pkiroot'                                              # PKI root folder
+$ca = 'CA'                                                                 # name of the CA
+$LOGFILE = ".\LOG_CertReq_PS.txt"                                          # Log file location
