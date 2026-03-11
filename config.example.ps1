7418c1a1cfadd71d28891d24c8b28275054cae8c
Sargon1729 <136961479+Sargon1729@users.noreply.github.com>
2026-03-11 10:12:38 +0000

    Add example PowerShell configuration file

---
+++ config.example.ps1
@@ -0,0 +1,5 @@
+$PKIRoot = '\\bss-pri-qna01\backups\PKI'                            # PKI root folder
+$certfolder = ".\Signed Certs"                                      # signed certs folder
+$Completed_csr_path = '.\Completed CSR'                             # completed CSRs
+$ca = 'Barrett Steel Issuing CA'                                    # name of the CA
+$LOGFILE = ".\LOG_CertReq_PS.txt"                                   # Log file location
